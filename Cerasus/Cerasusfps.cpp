7320643662472037cf0c3b7c5c2b590367b8c387
Helium <1029535012@qq.com>
2018-06-12 12:58:56 +0800

    Cerasus v1.01a

---
+++ Cerasus/Cerasusfps.cpp
@@ -0,0 +1,344 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     All rights reserved.
+*
+* @file		Cerasusfps.cpp
+* @brief	This File is Cerasusfps Dynamic Link Library Project.
+* @author	alopex
+* @version	v1.00a
+* @date		2018-06-09	v1.00a	alopex	Create This File.
+*/
+#include "Cerasusfps.h"
+
+//Cerasusfps类用于绘制和显示当前游戏帧速率(fps)
+
+//------------------------------------------------------------------
+// @Function:	 Cerasusfps()
+// @Purpose: Cerasusfps构造函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CCerasusfps::CCerasusfps()
+{
+	m_bThreadSafe = true;
+	if (m_bThreadSafe) InitializeCriticalSection(&m_cs);
+
+	m_pFont = NULL;
+	m_ffps = 0.0f;
+}
+
+//------------------------------------------------------------------
+// @Function:	 Cerasusfps()
+// @Purpose: Cerasusfps析构函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CCerasusfps::~CCerasusfps()
+{
+	SAFE_DELETE(m_pFont);
+
+	if (m_bThreadSafe) DeleteCriticalSection(&m_cs);
+}
+
+//------------------------------------------------------------------
+// @Function:	 Cerasusfps()
+// @Purpose: Cerasusfps构造函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CCerasusfps::CCerasusfps(IDirect3DDevice9* pD3D9Device)
+{
+	m_bThreadSafe = true;
+	if (m_bThreadSafe) InitializeCriticalSection(&m_cs);
+
+	m_pFont = new DirectFont(pD3D9Device);
+	m_ffps = 0.0f;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsGetFont()
+// @Purpose: Cerasusfps获取字体类
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+DirectFont* CERASUSFPS_CALL CCerasusfps::CCerasusfpsGetFont() const
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	return m_pFont;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsGetFps()
+// @Purpose: Cerasusfps获取fps
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+float CERASUSFPS_CALL CCerasusfps::CCerasusfpsGetFps() const
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	return m_ffps;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsSetFont()
+// @Purpose: Cerasusfps设置字体类
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+VOID CERASUSFPS_CALL CCerasusfps::CCerasusfpsSetFont(DirectFont* pFont)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	m_pFont = pFont;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsReset()
+// @Purpose: Cerasusfps重置
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT CERASUSFPS_CALL CCerasusfps::CCerasusfpsReset()
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	HRESULT hr;
+
+	hr = m_pFont->DirectFontReset();
+	return hr;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsInit()
+// @Purpose: Cerasusfps初始化
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT CERASUSFPS_CALL CCerasusfps::CCerasusfpsInit(int nFontSize)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	HRESULT hr;
+
+	hr = m_pFont->DirectFontInit(nFontSize);
+
+	return hr;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsInit()
+// @Purpose: Cerasusfps初始化
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT CERASUSFPS_CALL CCerasusfps::CCerasusfpsInit(int nFontSize, LPWSTR lpszFontType)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	HRESULT hr;
+
+	hr = m_pFont->DirectFontInit(nFontSize, lpszFontType);
+	
+	return hr;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsGetfps()
+// @Purpose: Cerasusfps计算FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+VOID CERASUSFPS_CALL CCerasusfps::CCerasusfpsGetfps()
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	static int FrameCount = 0;
+	static DWORD TimeLast = 0;
+	DWORD TimeCurrent = 0;
+	float TimeElapsed = 0.0f;
+
+	FrameCount++;
+	timeBeginPeriod(1);
+	TimeCurrent = timeGetTime();
+	timeEndPeriod(1);
+	TimeElapsed = (TimeCurrent - TimeLast) * 0.001f;
+
+	if (TimeElapsed >= 1.0f)
+	{
+		m_ffps = (float)(FrameCount * 1.0f / TimeElapsed);
+		TimeLast = TimeCurrent;
+		FrameCount = 0;
+	}
+
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsGetfpsEx()
+// @Purpose: Cerasusfps计算FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+VOID CERASUSFPS_CALL CCerasusfps::CCerasusfpsGetfpsEx(float fTimeDelta, float * pfps)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	static int FrameCount = 0;
+	static float TimeElapsed = 0.0f;
+
+	FrameCount++;
+	TimeElapsed += fTimeDelta;
+
+	if (TimeElapsed >= 1.0f)
+	{
+		*pfps = (float)(FrameCount * 1.0f / TimeElapsed);
+		FrameCount = 0;
+		TimeElapsed = 0.0f;
+	}
+
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsGetfpsEx()
+// @Purpose: Cerasusfps计算FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+VOID CERASUSFPS_CALL CCerasusfps::CCerasusfpsGetfpsEx(float * pfps)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	static int FrameCount = 0;
+	static DWORD TimeLast = 0;
+	DWORD TimeCurrent = 0;
+	float TimeElapsed = 0.0f;
+
+	FrameCount++;
+	timeBeginPeriod(1);
+	TimeCurrent = timeGetTime();
+	timeEndPeriod(1);
+	TimeElapsed = (TimeCurrent - TimeLast) * 0.001f;
+
+	if (TimeElapsed >= 1.0f)
+	{
+		*pfps = (float)(FrameCount * 1.0f / TimeElapsed);
+		TimeLast = TimeCurrent;
+		FrameCount = 0;
+	}
+
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsGetfpsEx()
+// @Purpose: Cerasusfps计算FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+float CERASUSFPS_CALL CCerasusfps::CCerasusfpsGetfpsEx(float fTimeDelta)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	static int FrameCount = 0;
+	static float TimeElapsed = 0.0f;
+	float fps = 0.0f;
+
+	FrameCount++;
+	TimeElapsed += fTimeDelta;
+
+	if (TimeElapsed >= 1.0f)
+	{
+		fps = (float)(FrameCount * 1.0f / TimeElapsed);
+		FrameCount = 0;
+		TimeElapsed = 0.0f;
+	}
+
+	return fps;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsGetfpsEx()
+// @Purpose: Cerasusfps计算FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+float CERASUSFPS_CALL CCerasusfps::CCerasusfpsGetfpsEx()
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	static int FrameCount = 0;
+	static DWORD TimeLast = 0;
+	DWORD TimeCurrent = 0;
+	float TimeElapsed = 0.0f;
+	float fps;
+
+	FrameCount++;
+	timeBeginPeriod(1);
+	TimeCurrent = timeGetTime();
+	timeEndPeriod(1);
+	TimeElapsed = (TimeCurrent - TimeLast) * 0.001f;
+
+	if (TimeElapsed >= 1.0f)
+	{
+		fps = (float)(FrameCount * 1.0f / TimeElapsed);
+		TimeLast = TimeCurrent;
+		FrameCount = 0;
+	}
+
+	return fps;
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsDrawfps()
+// @Purpose: Cerasusfps绘制FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+VOID CERASUSFPS_CALL CCerasusfps::CCerasusfpsDrawfps(HWND hWnd)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	wchar_t fpsArr[20];
+	int ArrSize;
+
+	ArrSize = swprintf_s(fpsArr, 20, _T("%0.1ffps"), m_ffps);
+	m_pFont->DirectFontDrawText(hWnd, fpsArr, DIRECTFONT_FORMAT_BOTTOMRIGHT, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsDrawfps()
+// @Purpose: Cerasusfps绘制FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+VOID CERASUSFPS_CALL CCerasusfps::CCerasusfpsDrawfps(HWND hWnd, DWORD Format)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	wchar_t fpsArr[20];
+	int ArrSize;
+
+	ArrSize = swprintf_s(fpsArr, 20, _T("%0.1ffps"), m_ffps);
+	m_pFont->DirectFontDrawText(hWnd, fpsArr, Format, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusfpsDrawfps()
+// @Purpose: Cerasusfps绘制FPS
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+VOID CERASUSFPS_CALL CCerasusfps::CCerasusfpsDrawfps(HWND hWnd, DWORD Format, D3DCOLOR Color)
+{
+	DirectThreadSafe ThreadSafe(&m_cs, m_bThreadSafe);
+	wchar_t fpsArr[20];
+	int ArrSize;
+
+	ArrSize = swprintf_s(fpsArr, 20, _T("%0.1ffps"), m_ffps);
+	m_pFont->DirectFontDrawText(hWnd, fpsArr, Format, Color);
+}
---
+++ Cerasus/Cerasusfps.h
@@ -0,0 +1,78 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     All rights reserved.
+*
+* @file		Cerasusfps.h
+* @brief	This File is Cerasusfps Dynamic Link Library Project.
+* @author	alopex
+* @version	v1.00a
+* @date		2018-06-09	v1.00a	alopex	Create This File.
+*/
+#pragma once
+
+#ifndef __CERASUSFPS_H_
+#define __CERASUSFPS_H_
+
+//Include DirectX Common Header File
+#include "DirectCommon.h"
+#include "DirectFont.h"
+#include "DirectThreadSafe.h"
+
+//Macro Definition
+#ifdef CERASUS_EXPORTS
+#define CERASUSFPS_API	__declspec(dllexport)
+#else
+#define CERASUSFPS_API	__declspec(dllimport)
+#endif
+
+#define CERASUSFPS_CALL	__stdcall
+
+//Class Definition
+class CERASUSFPS_API CCerasusfps
+{
+private:
+	DirectFont* m_pFont;	//Cerasusfps DirectFont(字体样式)
+
+	CRITICAL_SECTION m_cs;	//Cerasusfps Thread Safe(CriticalSection)
+	bool m_bThreadSafe;		//Cerasusfps Thread Safe Status
+	
+private:
+	float m_ffps;			//Cerasusfps Frame Per Second(帧速率)
+
+public:
+	CCerasusfps();			//Cerasusfps Constructor Function(构造函数)
+	~CCerasusfps();			//Cerasusfps Destructor Function(析构函数)
+
+	CCerasusfps(IDirect3DDevice9* pD3D9Device);	//Cerasusfps Constructor Function(构造函数)(重载+1)
+
+	//访问
+	DirectFont* CERASUSFPS_CALL CCerasusfpsGetFont() const;		//Cerasusfps Get Font(获取字体类)
+	float CERASUSFPS_CALL CCerasusfpsGetFps() const;			//Cerasusfps Get Fps(获取fps值)
+
+	//控制
+	VOID CERASUSFPS_CALL CCerasusfpsSetFont(DirectFont* pFont);	//Cerasusfps Set Font(设置字体类)
+
+	//重置
+	HRESULT CERASUSFPS_CALL CCerasusfpsReset();					//Cerasusfps Reset(D3D丢失设备Reset)
+
+	//初始化
+	HRESULT CERASUSFPS_CALL CCerasusfpsInit(int nFontSize);							//Cerasusfps Initialize(初始化)
+	HRESULT CERASUSFPS_CALL CCerasusfpsInit(int nFontSize, LPWSTR lpszFontType);	//Cerasusfps Initialize(初始化)(重载+1)
+
+	//计算
+	VOID CERASUSFPS_CALL CCerasusfpsGetfps();										//Cerasusfps Calculate Fps(计算fps)
+	VOID CERASUSFPS_CALL CCerasusfpsGetfpsEx(float fTimeDelta, float* pfps);		//Cerasusfps Calculate Fps Ex(计算fps)
+	VOID CERASUSFPS_CALL CCerasusfpsGetfpsEx(float* pfps);							//Cerasusfps Calculate Fps Ex(计算fps)(重载+1)
+	float CERASUSFPS_CALL CCerasusfpsGetfpsEx(float fTimeDelta);					//Cerasusfps Calculate Fps Ex(计算fps)(重载+2)
+	float CERASUSFPS_CALL CCerasusfpsGetfpsEx();									//Cerasusfps Calculate Fps Ex(计算fps)(重载+3)
+
+	//绘制
+	VOID CERASUSFPS_CALL CCerasusfpsDrawfps(HWND hWnd);									//Cerasusfps Draw Fps(绘制fps)
+	VOID CERASUSFPS_CALL CCerasusfpsDrawfps(HWND hWnd, DWORD Format);					//Cerasusfps Draw Fps(绘制fps)(重载+1)
+	VOID CERASUSFPS_CALL CCerasusfpsDrawfps(HWND hWnd, DWORD Format, D3DCOLOR Color);	//Cerasusfps Draw Fps(绘制fps)(重载+2)
+};
+
+
+#endif // !__CERASUSFPS_H_
+

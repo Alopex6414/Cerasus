1cc01bce399ec372cc6d8d736f8c3d76c191ee03
Alopex6414 <1029535012@qq.com>
2018-11-23 10:15:23 +0800

    Cerasus v1.1.0.42a 修改DirectSafe调用方式.

--- Cerasus/DirectSafe.cpp
+++ Cerasus/DirectSafe.cpp
@@ -6,11 +6,12 @@
 * @file		DirectSafe.cpp
 * @brief	This Program is DirectSafe DLL Project.
 * @author	Alopex/Helium
-* @version	v1.03a
+* @version	v1.04a
 * @date		2018-01-3	v1.00a	alopex	Create Project.
 * @date		2018-01-4	v1.01a	alopex	Modify inline Key Words.
 * @date		2018-04-12	v1.02a	alopex	Add Macro Call Mode.
 * @date		2018-06-22	v1.03a	alopex	Add Version Infomation.
+* @date		2018-11-23	v1.04a	alopex	Alter Call Method.
 */
 #include "DirectCommon.h"
 #include "DirectSafe.h"
@@ -50,7 +51,7 @@ DirectSafe::~DirectSafe()
 // @Para: None
 // @Return: None
 //------------------------------------------------------------------
-void DIRECTSAFE_CALLMODE DirectSafeInit()
+void DIRECTSAFE_CALLMETHOD DirectSafeInit()
 {
 	if (g_bDirectSafe) InitializeCriticalSection(&g_csDirect);
 }
@@ -62,7 +63,7 @@ void DIRECTSAFE_CALLMODE DirectSafeInit()
 // @Para: None
 // @Return: None
 //------------------------------------------------------------------
-void DIRECTSAFE_CALLMODE DirectSafeDelete()
+void DIRECTSAFE_CALLMETHOD DirectSafeDelete()
 {
 	if (g_bDirectSafe) DeleteCriticalSection(&g_csDirect);
 }
--- Cerasus/DirectSafe.h
+++ Cerasus/DirectSafe.h
@@ -6,11 +6,12 @@
 * @file		DirectSafe.h
 * @brief	This Program is DirectSafe DLL Project.
 * @author	Alopex/Helium
-* @version	v1.03a
+* @version	v1.04a
 * @date		2018-01-3	v1.00a	alopex	Create Project.
 * @date		2018-01-4	v1.01a	alopex	Modify inline Key Words.
 * @date		2018-04-12	v1.02a	alopex	Add Macro Call Mode.
 * @date		2018-06-22	v1.03a	alopex	Add Version Infomation.
+* @date		2018-11-23	v1.04a	alopex	Alter Call Method.
 */
 #pragma once
 
@@ -27,7 +28,7 @@
 #define DIRECTSAFE_API	__declspec(dllimport)
 #endif
 
-#define DIRECTSAFE_CALLMODE	__stdcall
+#define DIRECTSAFE_CALLMETHOD	__stdcall
 
 //Class Definition
 class DIRECTSAFE_API DirectSafe
@@ -42,7 +43,7 @@ extern "C" DIRECTSAFE_API CRITICAL_SECTION g_csDirect;
 extern "C" DIRECTSAFE_API bool g_bDirectSafe;
 
 //Function Definition
-extern "C" DIRECTSAFE_API void DIRECTSAFE_CALLMODE DirectSafeInit();
-extern "C" DIRECTSAFE_API void DIRECTSAFE_CALLMODE DirectSafeDelete();
+extern "C" DIRECTSAFE_API void DIRECTSAFE_CALLMETHOD DirectSafeInit();
+extern "C" DIRECTSAFE_API void DIRECTSAFE_CALLMETHOD DirectSafeDelete();
 
 #endif

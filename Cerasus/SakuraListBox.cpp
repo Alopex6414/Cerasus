083b5a2deac3d86b46131957eb58fe441f7be8c7
Alopex6414 <1029535012@qq.com>
2018-12-05 09:38:26 +0800

    Cerasus v1.1.5.66a 添加CSakuraListBox类.

---
+++ Cerasus/SakuraListBox.cpp
@@ -0,0 +1,739 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     All rights reserved.
+*
+* @file		SakuraListBox.cpp
+* @brief	This File is SakuraGUI DLL Project.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2018-12-04	v1.00a	alopex	Create Project.
+*/
+#include "SakuraDialog.h"
+#include "SakuraListBox.h"
+
+// SakuraGUI 列表框派生类(UI)
+
+//------------------------------------------------------------------
+// @Function:	 CSakuraListBox()
+// @Purpose: CSakuraListBox构造函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CSakuraListBox::CSakuraListBox(CSakuraDialog* pDialog)
+{
+	m_eType = SAKURA_CONTROL_LISTBOX;
+	m_pDialog = pDialog;
+
+	m_dwStyle = 0;
+	m_nSBWidth = 16;
+	m_nSelected = -1;
+	m_nSelStart = 0;
+	m_bDrag = false;
+	m_nBorder = 6;
+	m_nMargin = 5;
+	m_nTextHeight = 0;
+}
+
+//------------------------------------------------------------------
+// @Function:	 ~CSakuraListBox()
+// @Purpose: CSakuraListBox析构函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CSakuraListBox::~CSakuraListBox()
+{
+	RemoveAllItems();
+}
+
+//------------------------------------------------------------------
+// @Function:	 OnInit()
+// @Purpose: CSakuraListBox控件初始化
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT SAKURALISTBOX_CALLMETHOD CSakuraListBox::OnInit()
+{
+	return m_pDialog->InitControl(&m_ScrollBar);
+}
+
+//------------------------------------------------------------------
+// @Function:	 CanHaveFocus()
+// @Purpose: CSakuraListBox是否拥有焦点
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+bool SAKURALISTBOX_CALLMETHOD CSakuraListBox::CanHaveFocus()
+{
+	return (m_bVisible && m_bEnabled);
+}
+
+//------------------------------------------------------------------
+// @Function:	 HandleKeyboard()
+// @Purpose: CSakuraListBox键盘消息响应
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+bool SAKURALISTBOX_CALLMETHOD CSakuraListBox::HandleKeyboard(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (!m_bEnabled || !m_bVisible)
+	{
+		return false;
+	}
+
+	if (m_ScrollBar.HandleKeyboard(uMsg, wParam, lParam))
+	{
+		return true;
+	}
+
+	switch (uMsg)
+	{
+		case WM_KEYDOWN:
+			switch (wParam)
+			{
+				case VK_UP:
+				case VK_DOWN:
+				case VK_NEXT:
+				case VK_PRIOR:
+				case VK_HOME:
+				case VK_END:
+				{
+					if (m_Items.size() == 0)
+					{
+						return true;
+					}
+
+					int nOldSelected = m_nSelected;
+
+					switch (wParam)
+					{
+						case VK_UP:
+						{
+							--m_nSelected; 
+							break;
+						}
+						case VK_DOWN:
+						{
+							++m_nSelected; 
+							break;
+						}
+						case VK_NEXT:
+						{
+							m_nSelected += m_ScrollBar.GetPageSize() - 1;
+							break;
+						}
+						case VK_PRIOR:
+						{
+							m_nSelected -= m_ScrollBar.GetPageSize() - 1;
+							break;
+						}
+						case VK_HOME:
+						{
+							m_nSelected = 0; 
+							break;
+						}
+						case VK_END:
+						{
+							m_nSelected = m_Items.size() - 1;
+							break;
+						}
+
+					}
+
+					if (m_nSelected < 0)
+					{
+						m_nSelected = 0;
+					}
+					if (m_nSelected >= (int)m_Items.size())
+					{
+						m_nSelected = m_Items.size() - 1;
+					}
+
+					if (nOldSelected != m_nSelected)
+					{
+						if (m_dwStyle & MULTISELECTION)
+						{
+							for (int i = 0; i < (int)m_Items.size(); ++i)
+							{
+								CSakuraListBoxItem* pItem = m_Items[i];
+								pItem->bSelected = false;
+							}
+
+							if (GetKeyState(VK_SHIFT) < 0)
+							{
+								int nEnd = __max(m_nSelStart, m_nSelected);
+
+								for (int n = __min(m_nSelStart, m_nSelected); n <= nEnd; ++n)
+								{
+									m_Items[n]->bSelected = true;
+								}
+							}
+							else
+							{
+								m_Items[m_nSelected]->bSelected = true;
+
+								m_nSelStart = m_nSelected;
+							}
+						}
+						else
+						{
+							m_nSelStart = m_nSelected;
+						}
+
+						m_ScrollBar.ShowItem(m_nSelected);
+
+						m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+					}
+					return true;
+				}
+
+				case VK_SPACE:
+					m_pDialog->SendEvent(EVENT_LISTBOX_ITEM_DBLCLK, true, this);
+					return true;
+			}
+			break;
+	}
+
+	return false;
+}
+
+//------------------------------------------------------------------
+// @Function:	 HandleMouse()
+// @Purpose: CSakuraListBox鼠标消息响应
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+bool SAKURALISTBOX_CALLMETHOD CSakuraListBox::HandleMouse(UINT uMsg, POINT pt, WPARAM wParam, LPARAM lParam)
+{
+	if (!m_bEnabled || !m_bVisible)
+	{
+		return false;
+	}
+
+	if (WM_LBUTTONDOWN == uMsg)
+	{
+		if (!m_bHasFocus)
+		{
+			m_pDialog->RequestFocus(this);
+		}
+	}
+
+	if (m_ScrollBar.HandleMouse(uMsg, pt, wParam, lParam))
+	{
+		return true;
+	}
+
+	switch (uMsg)
+	{
+		case WM_LBUTTONDOWN:
+		case WM_LBUTTONDBLCLK:
+			if (m_Items.size() > 0 && PtInRect(&m_rcSelection, pt))
+			{
+				int nClicked;
+
+				if (m_nTextHeight)
+				{
+					nClicked = m_ScrollBar.GetTrackPos() + (pt.y - m_rcText.top) / m_nTextHeight;
+				}
+				else
+				{
+					nClicked = -1;
+				}
+
+				if (nClicked >= m_ScrollBar.GetTrackPos() && nClicked < (int)m_Items.size() && nClicked < m_ScrollBar.GetTrackPos() + m_ScrollBar.GetPageSize())
+				{
+					m_bDrag = true;
+
+					if (uMsg == WM_LBUTTONDBLCLK)
+					{
+						m_pDialog->SendEvent(EVENT_LISTBOX_ITEM_DBLCLK, true, this);
+						return true;
+					}
+
+					m_nSelected = nClicked;
+					if (!(wParam & MK_SHIFT))
+					{
+						m_nSelStart = m_nSelected;
+					}
+
+					if (m_dwStyle & MULTISELECTION)
+					{
+						CSakuraListBoxItem* pSelItem = m_Items.at(m_nSelected);
+						if ((wParam & (MK_SHIFT | MK_CONTROL)) == MK_CONTROL)
+						{
+							pSelItem->bSelected = !pSelItem->bSelected;
+						}
+						else if ((wParam & (MK_SHIFT | MK_CONTROL)) == MK_SHIFT)
+						{
+							int nBegin = __min(m_nSelStart, m_nSelected);
+							int nEnd = __max(m_nSelStart, m_nSelected);
+
+							for (int i = 0; i < nBegin; ++i)
+							{
+								CSakuraListBoxItem* pItem = m_Items.at(i);
+								pItem->bSelected = false;
+							}
+
+							for (int i = nEnd + 1; i < (int)m_Items.size(); ++i)
+							{
+								CSakuraListBoxItem* pItem = m_Items.at(i);
+								pItem->bSelected = false;
+							}
+
+							for (int i = nBegin; i <= nEnd; ++i)
+							{
+								CSakuraListBoxItem* pItem = m_Items.at(i);
+								pItem->bSelected = true;
+							}
+						}
+						else if ((wParam & (MK_SHIFT | MK_CONTROL)) == (MK_SHIFT | MK_CONTROL))
+						{
+							int nBegin = __min(m_nSelStart, m_nSelected);
+							int nEnd = __max(m_nSelStart, m_nSelected);
+
+							bool bLastSelected = m_Items.at(m_nSelStart)->bSelected;
+							for (int i = nBegin + 1; i < nEnd; ++i)
+							{
+								CSakuraListBoxItem* pItem = m_Items.at(i);
+								pItem->bSelected = bLastSelected;
+							}
+
+							pSelItem->bSelected = true;
+
+							m_nSelected = m_nSelStart;
+						}
+						else
+						{
+							for (int i = 0; i < (int)m_Items.size(); ++i)
+							{
+								CSakuraListBoxItem* pItem = m_Items.at(i);
+								pItem->bSelected = false;
+							}
+
+							pSelItem->bSelected = true;
+						}
+					}
+
+					m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+				}
+
+				return true;
+			}
+			break;
+
+		case WM_LBUTTONUP:
+		{
+			m_bDrag = false;
+
+			if (m_nSelected != -1)
+			{
+				int nEnd = __max(m_nSelStart, m_nSelected);
+
+				for (int n = __min(m_nSelStart, m_nSelected) + 1; n < nEnd; ++n)
+				{
+					m_Items[n]->bSelected = m_Items[m_nSelStart]->bSelected;
+				}
+
+				m_Items[m_nSelected]->bSelected = m_Items[m_nSelStart]->bSelected;
+
+				if (m_nSelStart != m_nSelected)
+				{
+					m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+				}
+
+				m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION_END, true, this);
+			}
+			return false;
+		}
+
+		case WM_MOUSEMOVE:
+			if (m_bDrag)
+			{
+				int nItem;
+				if (m_nTextHeight)
+				{
+					nItem = m_ScrollBar.GetTrackPos() + (pt.y - m_rcText.top) / m_nTextHeight;
+				}
+				else
+				{
+					nItem = -1;
+				}
+
+				if (nItem >= (int)m_ScrollBar.GetTrackPos() && nItem < (int)m_Items.size() && nItem < m_ScrollBar.GetTrackPos() + m_ScrollBar.GetPageSize())
+				{
+					m_nSelected = nItem;
+					m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+				}
+				else if (nItem < (int)m_ScrollBar.GetTrackPos())
+				{
+					m_ScrollBar.Scroll(-1);
+					m_nSelected = m_ScrollBar.GetTrackPos();
+					m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+				}
+				else if (nItem >= m_ScrollBar.GetTrackPos() + m_ScrollBar.GetPageSize())
+				{
+					m_ScrollBar.Scroll(1);
+					m_nSelected = __min((int)m_Items.size(), m_ScrollBar.GetTrackPos() + m_ScrollBar.GetPageSize()) - 1;
+					m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+				}
+			}
+			break;
+
+		case WM_MOUSEWHEEL:
+		{
+			UINT uLines;
+			SystemParametersInfo(SPI_GETWHEELSCROLLLINES, 0, &uLines, 0);
+			int nScrollAmount = int((short)HIWORD(wParam)) / WHEEL_DELTA * uLines;
+			m_ScrollBar.Scroll(-nScrollAmount);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+//------------------------------------------------------------------
+// @Function:	 MsgProc()
+// @Purpose: CSakuraListBox消息处理函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+bool SAKURALISTBOX_CALLMETHOD CSakuraListBox::MsgProc(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return false;
+}
+
+//------------------------------------------------------------------
+// @Function:	 Render()
+// @Purpose: CSakuraListBox渲染控件
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::Render()
+{
+}
+
+//------------------------------------------------------------------
+// @Function:	 UpdateRects()
+// @Purpose: CSakuraListBox更新控件区域
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::UpdateRects()
+{
+	CSakuraControl::UpdateRects();
+
+	m_rcSelection = m_rcBoundingBox;
+	m_rcSelection.right -= m_nSBWidth;
+	InflateRect(&m_rcSelection, -m_nBorder, -m_nBorder);
+	m_rcText = m_rcSelection;
+	InflateRect(&m_rcText, -m_nMargin, 0);
+
+	m_ScrollBar.SetLocation(m_rcBoundingBox.right - m_nSBWidth, m_rcBoundingBox.top);
+	m_ScrollBar.SetSize(m_nSBWidth, m_nHeight);
+	CUFont* pFontNode = m_pDialog->GetManager()->GetFontNode(0);
+	if (pFontNode && pFontNode->nFontSize)
+	{
+		m_ScrollBar.SetPageSize((m_rcText.bottom - m_rcText.top) / pFontNode->nFontSize);
+
+		m_ScrollBar.ShowItem(m_nSelected);
+	}
+}
+
+//------------------------------------------------------------------
+// @Function:	 GetStyle()
+// @Purpose: CSakuraListBox获取列表框样式
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+DWORD SAKURALISTBOX_CALLMETHOD CSakuraListBox::GetStyle() const
+{
+	return m_dwStyle;
+}
+
+//------------------------------------------------------------------
+// @Function:	 GetSize()
+// @Purpose: CSakuraListBox获取长度
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+int SAKURALISTBOX_CALLMETHOD CSakuraListBox::GetSize() const
+{
+	return m_Items.size();
+}
+
+//------------------------------------------------------------------
+// @Function:	 SetStyle()
+// @Purpose: CSakuraListBox设置列表框样式
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::SetStyle(DWORD dwStyle)
+{
+	m_dwStyle = dwStyle;
+}
+
+//------------------------------------------------------------------
+// @Function:	 GetScrollBarWidth()
+// @Purpose: CSakuraListBox获取滚动条宽度
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+int SAKURALISTBOX_CALLMETHOD CSakuraListBox::GetScrollBarWidth() const
+{
+	return m_nSBWidth;
+}
+
+//------------------------------------------------------------------
+// @Function:	 SetScrollBarWidth()
+// @Purpose: CSakuraListBox设置滚动条宽度
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::SetScrollBarWidth(int nWidth)
+{
+	m_nSBWidth = nWidth; 
+	UpdateRects();
+}
+
+//------------------------------------------------------------------
+// @Function:	 SetBorder()
+// @Purpose: CSakuraListBox设置边框宽度
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::SetBorder(int nBorder, int nMargin)
+{
+	m_nBorder = nBorder; 
+	m_nMargin = nMargin;
+}
+
+//------------------------------------------------------------------
+// @Function:	 AddItem()
+// @Purpose: CSakuraListBox添加项
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT SAKURALISTBOX_CALLMETHOD CSakuraListBox::AddItem(const WCHAR * wszText, void * pData)
+{
+	CSakuraListBoxItem* pNewItem = new CSakuraListBoxItem();
+	if (!pNewItem)
+	{
+		return E_OUTOFMEMORY;
+	}
+
+	wcscpy_s(pNewItem->strText, 256, wszText);
+	pNewItem->pData = pData;
+	SetRect(&pNewItem->rcActive, 0, 0, 0, 0);
+	pNewItem->bSelected = false;
+
+	m_Items.push_back(pNewItem);
+	m_ScrollBar.SetTrackRange(0, m_Items.size());
+
+	return S_OK;
+}
+
+//------------------------------------------------------------------
+// @Function:	 InsertItem()
+// @Purpose: CSakuraListBox插入项
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT SAKURALISTBOX_CALLMETHOD CSakuraListBox::InsertItem(int nIndex, const WCHAR * wszText, void * pData)
+{
+	CSakuraListBoxItem* pNewItem = new CSakuraListBoxItem();
+	if (!pNewItem)
+	{
+		return E_OUTOFMEMORY;
+	}
+
+	wcscpy_s(pNewItem->strText, 256, wszText);
+	pNewItem->pData = pData;
+	SetRect(&pNewItem->rcActive, 0, 0, 0, 0);
+	pNewItem->bSelected = false;
+
+	m_Items.insert(m_Items.begin() + nIndex, pNewItem);
+	m_ScrollBar.SetTrackRange(0, m_Items.size());
+
+	return S_OK;
+}
+
+//------------------------------------------------------------------
+// @Function:	 RemoveItem()
+// @Purpose: CSakuraListBox移除项
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::RemoveItem(int nIndex)
+{
+	if (nIndex < 0 || nIndex >= (int)m_Items.size())
+	{
+		return;
+	}
+
+	CSakuraListBoxItem* pItem = m_Items.at(nIndex);
+	SAFE_DELETE(pItem);
+
+	m_Items.erase(m_Items.begin() + nIndex);
+	m_ScrollBar.SetTrackRange(0, m_Items.size());
+	if (m_nSelected >= (int)m_Items.size())
+	{
+		m_nSelected = m_Items.size() - 1;
+	}
+
+	m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+}
+
+//------------------------------------------------------------------
+// @Function:	 RemoveItemByData()
+// @Purpose: CSakuraListBox移除项(值索引)
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::RemoveItemByData(void * pData)
+{
+	
+}
+
+//------------------------------------------------------------------
+// @Function:	 RemoveAllItems()
+// @Purpose: CSakuraListBox移除所有项
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::RemoveAllItems()
+{
+	for (int i = 0; i < m_Items.size(); ++i)
+	{
+		CSakuraListBoxItem* pItem = m_Items.at(i);
+		SAFE_DELETE(pItem);
+	}
+
+	m_Items.clear();
+	m_ScrollBar.SetTrackRange(0, 1);
+}
+
+//------------------------------------------------------------------
+// @Function:	 GetItem()
+// @Purpose: CSakuraListBox获取值
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CSakuraListBoxItem *SAKURALISTBOX_CALLMETHOD CSakuraListBox::GetItem(int nIndex)
+{
+	if (nIndex < 0 || nIndex >= (int)m_Items.size())
+	{
+		return NULL;
+	}
+
+	return m_Items[nIndex];
+}
+
+//------------------------------------------------------------------
+// @Function:	 GetSelectedIndex()
+// @Purpose: CSakuraListBox获取值选中索引
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+int SAKURALISTBOX_CALLMETHOD CSakuraListBox::GetSelectedIndex(int nPreviousSelected)
+{
+	if (nPreviousSelected < -1)
+	{
+		return -1;
+	}
+
+	if (m_dwStyle & MULTISELECTION)
+	{
+		for (int i = nPreviousSelected + 1; i < (int)m_Items.size(); ++i)
+		{
+			CSakuraListBoxItem* pItem = m_Items.at(i);
+
+			if (pItem->bSelected)
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+	else
+	{
+		return m_nSelected;
+	}
+}
+
+//------------------------------------------------------------------
+// @Function:	 GetSelectedItem()
+// @Purpose: CSakuraListBox获取值选中项
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CSakuraListBoxItem *SAKURALISTBOX_CALLMETHOD CSakuraListBox::GetSelectedItem(int nPreviousSelected)
+{
+	return GetItem(GetSelectedIndex(nPreviousSelected));
+}
+
+//------------------------------------------------------------------
+// @Function:	 SelectItem()
+// @Purpose: CSakuraListBox选中项
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void SAKURALISTBOX_CALLMETHOD CSakuraListBox::SelectItem(int nNewIndex)
+{
+	if (m_Items.size() == 0)
+	{
+		return;
+	}
+
+	int nOldSelected = m_nSelected;
+
+	m_nSelected = nNewIndex;
+
+	if (m_nSelected < 0)
+	{
+		m_nSelected = 0;
+	}
+	if (m_nSelected >= (int)m_Items.size())
+	{
+		m_nSelected = m_Items.size() - 1;
+	}
+
+	if (nOldSelected != m_nSelected)
+	{
+		if (m_dwStyle & MULTISELECTION)
+		{
+			m_Items[m_nSelected]->bSelected = true;
+		}
+
+		m_nSelStart = m_nSelected;
+
+		m_ScrollBar.ShowItem(m_nSelected);
+	}
+
+	m_pDialog->SendEvent(EVENT_LISTBOX_SELECTION, true, this);
+}
---
+++ Cerasus/SakuraListBox.h
@@ -0,0 +1,99 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     All rights reserved.
+*
+* @file		SakuraListBox.h
+* @brief	This File is SakuraGUI DLL Project.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2018-12-04	v1.00a	alopex	Create Project.
+*/
+#pragma once
+
+#ifndef __SAKURALISTBOX_H_
+#define __SAKURALISTBOX_H_
+
+//Include SakuraGUI Common Header File
+#include "SakuraUICommon.h"
+#include "SakuraControl.h"
+#include "SakuraScrollBar.h"
+
+//Macro Definition
+#ifdef	CERASUS_EXPORTS
+#define SAKURALISTBOX_API	__declspec(dllexport)
+#else
+#define SAKURALISTBOX_API	__declspec(dllimport)
+#endif
+
+#define	SAKURALISTBOX_CALLMETHOD	__stdcall
+
+//Struct Definition
+typedef struct
+{
+	WCHAR strText[256];
+	void* pData;
+	RECT rcActive;
+	bool bSelected;
+} CSakuraListBoxItem, *LPCSakuraListBoxItem;
+
+//Class Definition
+class SAKURALISTBOX_API CSakuraListBox : public CSakuraControl
+{
+protected:
+	RECT m_rcText;							// CSakuraListBox 文本框
+	RECT m_rcSelection;						// CSakuraListBox 文本选择框
+	CSakuraScrollBar m_ScrollBar;			// CSakuraListBox 滚动条
+
+	int m_nSBWidth;							// CSakuraListBox 滚动条宽度
+	int m_nBorder;							// CSakuraListBox 边框宽度
+	int m_nMargin;
+	int m_nTextHeight;						// CSakuraListBox 文本宽度
+	DWORD m_dwStyle;						// CSakuraListBox 列表框样式
+	int m_nSelected;						// CSakuraListBox 列表框选中项
+	int m_nSelStart;						// CSakuraListBox 列表框开始项
+	bool m_bDrag;							// CSakuraListBox 列表框拖拽
+
+	vector<CSakuraListBoxItem*> m_Items;	// CSakuraListBox 列表框内容
+
+public:
+	enum STYLE								// CSakuraListBox 列表框样式
+	{
+		MULTISELECTION = 1
+	};
+	
+public:
+			CSakuraListBox(CSakuraDialog* pDialog = NULL);			// CSakuraListBox 构造函数
+	virtual	~CSakuraListBox();										// CSakuraListBox 析构函数
+
+	virtual	HRESULT	SAKURALISTBOX_CALLMETHOD	OnInit();			// CSakuraListBox 初始化控件
+	virtual bool    SAKURALISTBOX_CALLMETHOD	CanHaveFocus();		// CSakuraListBox 是否拥有焦点
+
+	virtual bool    SAKURALISTBOX_CALLMETHOD	HandleKeyboard(UINT uMsg, WPARAM wParam, LPARAM lParam);			// CSakuraListBox 键盘消息响应
+	virtual bool    SAKURALISTBOX_CALLMETHOD	HandleMouse(UINT uMsg, POINT pt, WPARAM wParam, LPARAM lParam);		// CSakuraListBox 鼠标消息响应
+	virtual bool    SAKURALISTBOX_CALLMETHOD	MsgProc(UINT uMsg, WPARAM wParam, LPARAM lParam);					// CSakuraListBox 消息处理函数
+
+	virtual void    SAKURALISTBOX_CALLMETHOD	Render();							// CSakuraListBox 渲染控件
+	virtual void    SAKURALISTBOX_CALLMETHOD	UpdateRects();						// CSakuraListBox 更新控件区域
+
+	DWORD           SAKURALISTBOX_CALLMETHOD	GetStyle() const;					// CSakuraListBox 获取样式
+	int             SAKURALISTBOX_CALLMETHOD	GetSize() const;					// CSakuraListBox 获取长度
+	void            SAKURALISTBOX_CALLMETHOD	SetStyle(DWORD dwStyle);			// CSakuraListBox 设置样式
+	int             SAKURALISTBOX_CALLMETHOD	GetScrollBarWidth() const;			// CSakuraListBox 获取滚动条宽度
+	void            SAKURALISTBOX_CALLMETHOD	SetScrollBarWidth(int nWidth);		// CSakuraListBox 设置滚动条宽度
+	void            SAKURALISTBOX_CALLMETHOD	SetBorder(int nBorder, int nMargin);// CSakuraListBox 设置边框宽度
+
+	HRESULT         SAKURALISTBOX_CALLMETHOD	AddItem(const WCHAR* wszText, void* pData);							// CSakuraListBox 添加项
+	HRESULT         SAKURALISTBOX_CALLMETHOD	InsertItem(int nIndex, const WCHAR* wszText, void* pData);			// CSakuraListBox 插入项
+	void            SAKURALISTBOX_CALLMETHOD	RemoveItem(int nIndex);												// CSakuraListBox 移除项
+	void            SAKURALISTBOX_CALLMETHOD	RemoveItemByData(void* pData);										// CSakuraListBox 移除项(值索引)
+	void            SAKURALISTBOX_CALLMETHOD	RemoveAllItems();													// CSakuraListBox 移除所有项
+
+	CSakuraListBoxItem*	SAKURALISTBOX_CALLMETHOD	GetItem(int nIndex);											// CSakuraListBox 获取值
+	int             SAKURALISTBOX_CALLMETHOD		GetSelectedIndex(int nPreviousSelected = -1);					// CSakuraListBox 获取选中索引
+	CSakuraListBoxItem* SAKURALISTBOX_CALLMETHOD	GetSelectedItem(int nPreviousSelected = -1);					// CSakuraListBox 获取选中项
+	void            SAKURALISTBOX_CALLMETHOD		SelectItem(int nNewIndex);										// CSakuraListBox 选中项
+
+};
+
+#endif // !__SAKURALISTBOX_H_

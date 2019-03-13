77d9d63394a6bb95988a3ceb512e03ad696dc4be
Alopex6414 <1029535012@qq.com>
2019-03-13 16:02:20 +0800

    Cerasus v1.3.7.86a Add DXUIWindow Class.

--- Cerasus/DXUICommon.h
+++ Cerasus/DXUICommon.h
@@ -10,3 +10,40 @@
 * @date		2019-03-13	v1.00a	alopex	Create Project.
 */
 #pragma once
+
+#ifndef __DXUICOMMON_H_
+#define __DXUICOMMON_H_
+
+// Include DirectX Common Header File
+#include "DirectCommon.h"
+#include "DirectGraphics.h"
+#include "DirectGraphics2D.h"
+#include "DirectGraphics3D.h"
+#include "DirectInput.h"
+#include "DirectSound.h"
+#include "DirectShow.h"
+#include "DirectFont.h"
+#include "DirectSprite.h"
+#include "DirectSurface.h"
+#include "DirectTexture.h"
+#include "DirectMesh.h"
+#include "DirectSafe.h"
+#include "DirectThreadSafe.h"
+
+// Include Cerasus Common Header File
+#include "CerasusAlgorithm.h"
+#include "Cerasusfps.h"
+#include "CerasusUnit.h"
+
+// Include C/C++ Standard Header File
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+
+// Using C/C++ Class Namespace
+using namespace std;
+
+#endif // !__DXUICOMMON_H_

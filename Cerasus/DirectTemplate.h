bc2c5d9e06c179f8424cbfad617dadcf90b27d8b
Alopex6414 <1029535012@qq.com>
2019-03-30 16:06:29 +0800

    Cerasus v1.4.8.88a Add DirectTemplate.h.

---
+++ Cerasus/DirectTemplate.h
@@ -0,0 +1,54 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017~2019, Sakura&Fantasy
+*     All rights reserved.
+*
+* @file		DirectTemplate.h
+* @brief	This File is DirectX Library Template Header.
+* @author	Alopex/Alice
+* @version	v1.00a
+* @date		2019-03-30	v1.00a	alopex	Create File.
+*/
+#pragma once
+
+#ifndef __DIRECTTEMPLATE_H_
+#define __DIRECTTEMPLATE_H_
+
+// Include C/C++ Running Header File
+#include <iostream>
+#include <fstream>
+
+// Template Definition
+template <typename T>
+inline void Safe_Release(T* &p)
+{
+	if (p != nullptr)
+	{
+		p->Release();
+		p = nullptr;
+	}
+}
+
+template <typename T>
+inline void Safe_Delete(T* &p)
+{
+	if (p != nullptr)
+	{
+		delete p;
+		p = nullptr;
+	}
+}
+
+template <typename T>
+inline void Safe_Delete_Array(T* &p)
+{
+	if (p != nullptr)
+	{
+		delete[] p;
+		p = nullptr;
+	}
+}
+
+
+#endif // !__DIRECTTEMPLATE_H_
+

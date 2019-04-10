54b1d89edc3bff11d9790f4edd8c053d7f62f4db
Alopex6414 <1029535012@qq.com>
2019-04-10 21:35:07 +0800

    DirectSafe Modify Method.

--- Cerasus/DirectSafe.cpp
+++ Cerasus/DirectSafe.cpp
@@ -1,24 +1,26 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Sakura&Fantasy
 *     All rights reserved.
 *
 * @file		DirectSafe.cpp
 * @brief	This Program is DirectSafe DLL Project.
-* @author	Alopex/Helium
-* @version	v1.04a
+* @author	Alopex/Alice
+* @version	v1.05a
 * @date		2018-01-3	v1.00a	alopex	Create Project.
 * @date		2018-01-4	v1.01a	alopex	Modify inline Key Words.
 * @date		2018-04-12	v1.02a	alopex	Add Macro Call Mode.
 * @date		2018-06-22	v1.03a	alopex	Add Version Infomation.
 * @date		2018-11-23	v1.04a	alopex	Alter Call Method.
+* @date		2019-04-10	v1.05a	alopex	Add Notes.
 */
 #include "DirectCommon.h"
 #include "DirectSafe.h"
 
-//DirectSafe主要用于DirectX类库线程安全(~临界区)
 CRITICAL_SECTION g_csDirect;
 bool g_bDirectSafe = true;
+
+// DirectX9 Thread Safe(DirectX 线程安全)
 
 //------------------------------------------------------------------
 // @Function:	 DirectSafe()
--- Cerasus/DirectSafe.h
+++ Cerasus/DirectSafe.h
@@ -1,27 +1,29 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Sakura&Fantasy
 *     All rights reserved.
 *
 * @file		DirectSafe.h
 * @brief	This Program is DirectSafe DLL Project.
-* @author	Alopex/Helium
-* @version	v1.04a
+* @author	Alopex/Alice
+* @version	v1.05a
 * @date		2018-01-3	v1.00a	alopex	Create Project.
 * @date		2018-01-4	v1.01a	alopex	Modify inline Key Words.
 * @date		2018-04-12	v1.02a	alopex	Add Macro Call Mode.
 * @date		2018-06-22	v1.03a	alopex	Add Version Infomation.
 * @date		2018-11-23	v1.04a	alopex	Alter Call Method.
+* @date		2019-04-10	v1.05a	alopex	Add Notes.
 */
 #pragma once
 
 #ifndef __DIRECTSAFE_H_
 #define __DIRECTSAFE_H_
 
-//Include DirectX Common Header File
+// Include DirectX Common Header File
 #include "DirectCommon.h"
+#include "DirectTypes.h"
 
-//Macro Definition
+// Macro Definition
 #ifdef  CERASUS_EXPORTS
 #define DIRECTSAFE_API	__declspec(dllexport)
 #else
@@ -30,7 +32,7 @@
 
 #define DIRECTSAFE_CALLMETHOD	__stdcall
 
-//Class Definition
+// Class Definition
 class DIRECTSAFE_API DirectSafe
 {
 public:
@@ -38,11 +40,11 @@ public:
 	~DirectSafe();
 };
 
-//Variable Definition
+// Variable Definition
 extern "C" DIRECTSAFE_API CRITICAL_SECTION g_csDirect;
 extern "C" DIRECTSAFE_API bool g_bDirectSafe;
 
-//Function Definition
+// Function Definition
 extern "C" DIRECTSAFE_API void DIRECTSAFE_CALLMETHOD DirectSafeInit();
 extern "C" DIRECTSAFE_API void DIRECTSAFE_CALLMETHOD DirectSafeDelete();
 

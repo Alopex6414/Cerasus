abea648b11e5cca373be2b7c304f93d4e5fac98c
Alopex6414 <1029535012@qq.com>
2019-04-18 21:56:16 +0800

    Cerasus v1.4.8.153a. modify DirectTexture class method.

--- Cerasus/DirectThreadSafe.cpp
+++ Cerasus/DirectThreadSafe.cpp
@@ -1,17 +1,18 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Team Gorgeous Bubble
 *     All rights reserved.
 *
 * @file		DirectThreadSafe.cpp
 * @brief	This Program is DirectThreadSafe Header File.
-* @author	Alopex/Helium
-* @version	v1.00a
-* @date		2018-1-10	v1.00a	alopex	Create This File.
+* @author	Alopex/Alice
+* @version	v1.01a
+* @date		2018-01-10	v1.00a	alopex	Create This File.
+* @date		2019-04-18	v1.01a	alopex	Add Notes.
 */
 #include "DirectThreadSafe.h"
 
-//DirectThreadSafe构造函数
+// DirectThreadSafe构造函数
 DirectThreadSafe::DirectThreadSafe(const CRITICAL_SECTION* pCriticalSection, const bool bThreadSafe)
 {
 	m_pCriticalSection = (CRITICAL_SECTION*)pCriticalSection;
@@ -20,7 +21,7 @@ DirectThreadSafe::DirectThreadSafe(const CRITICAL_SECTION* pCriticalSection, con
 	if (m_bThreadSafe) EnterCriticalSection(m_pCriticalSection);
 }
 
-//DirectThreadSafe析构函数
+// DirectThreadSafe析构函数
 DirectThreadSafe::~DirectThreadSafe()
 {
 	if (m_bThreadSafe) LeaveCriticalSection(m_pCriticalSection);
--- Cerasus/DirectThreadSafe.h
+++ Cerasus/DirectThreadSafe.h
@@ -1,35 +1,36 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Team Gorgeous Bubble
 *     All rights reserved.
 *
 * @file		DirectThreadSafe.h
 * @brief	This Program is DirectThreadSafe Header File.
-* @author	Alopex/Helium
-* @version	v1.00a
-* @date		2018-1-10	v1.00a	alopex	Create This File.
+* @author	Alopex/Alice
+* @version	v1.01a
+* @date		2018-01-10	v1.00a	alopex	Create This File.
+* @date		2019-04-18	v1.01a	alopex	Add Notes.
 */
 #pragma once
 
 #ifndef __DIRECTTHREADSAFE_H_
 #define __DIRECTTHREADSAFE_H_
 
-//Include DirectCommon Header File
+// Include DirectX Common Header File
 #include "DirectCommon.h"
 
-//Macro Definition
+// Macro Definition
 #ifdef  CERASUS_EXPORTS
 #define DIRECTTHREADSAFE_API	__declspec(dllexport)
 #else
 #define DIRECTTHREADSAFE_API	__declspec(dllimport)
 #endif
 
-//Class
+// Class Definition
 class DIRECTTHREADSAFE_API DirectThreadSafe
 {
 private:
-	CRITICAL_SECTION* m_pCriticalSection;	//CriticalSection Variable
-	bool m_bThreadSafe;						//CriticalSection ThreadSafe Use Variable
+	CRITICAL_SECTION* m_pCriticalSection;					// CriticalSection Variable
+	bool m_bThreadSafe;										// CriticalSection ThreadSafe Use Variable
 
 public:
 	DirectThreadSafe(const CRITICAL_SECTION* pCriticalSection, const bool bThreadSafe);

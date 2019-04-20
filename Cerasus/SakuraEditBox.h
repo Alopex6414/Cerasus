a9f29384e7945a91768948e01cec27cc49b171f1
Alopex6414 <1029535012@qq.com>
2019-04-21 00:00:37 +0800

    Cerasus v1.5.9.169a. modify commit.

--- Cerasus/SakuraEditBox.h
+++ Cerasus/SakuraEditBox.h
@@ -1,24 +1,25 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Team Gorgeous Bubble
 *     All rights reserved.
 *
 * @file		SakuraEditBox.h
 * @brief	This File is SakuraGUI DLL Project.
-* @author	Alopex/Helium
-* @version	v1.00a
+* @author	Alopex/Alice
+* @version	v1.01a
 * @date		2018-12-04	v1.00a	alopex	Create Project.
+* @date		2019-04-20	v1.01a	alopex	Add Notes.
 */
 #pragma once
 
 #ifndef __SAKURAEDITBOX_H_
 #define __SAKURAEDITBOX_H_
 
-//Include SakuraGUI Common Header File
+// Include SakuraGUI Common Header File
 #include "SakuraUICommon.h"
 #include "SakuraControl.h"
 
-//Macro Definition
+// Macro Definition
 #ifdef	CERASUS_EXPORTS
 #define SAKURAEDITBOX_API	__declspec(dllexport)
 #else
@@ -27,7 +28,7 @@
 
 #define	SAKURAEDITBOX_CALLMETHOD	__stdcall
 
-//Class Definition
+// Class Definition
 class SAKURAEDITBOX_API CSakuraEditBox : public CSakuraControl
 {
 public:

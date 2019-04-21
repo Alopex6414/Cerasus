2be866372145fb55202a6e793faf572a2512d528
Alopex6414 <1029535012@qq.com>
2019-04-21 11:32:43 +0800

    Cerasus v1.5.9.178a. modify commit.

--- Cerasus/SakuraUICommon.h
+++ Cerasus/SakuraUICommon.h
@@ -1,20 +1,21 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Team Gorgeous Bubble
 *     All rights reserved.
 *
 * @file		SakuraUICommon.h
 * @brief	This File is SakuraGUI Common Header File.
 * @author	alopex
-* @version	v1.00a
+* @version	v1.01a
 * @date		2018-10-16	v1.00a	alopex	Create Project.
+* @date		2019-04-20	v1.01a	alopex	Add Notes.
 */
 #pragma once
 
 #ifndef __SAKURAUICOMMON_H_
 #define __SAKURAUICOMMON_H_
 
-//Include Direct Common Header File
+// Include Direct Common Header File
 #include "DirectCommon.h"
 #include "DirectFont.h"
 #include "DirectInput.h"
@@ -24,10 +25,10 @@
 #include "DirectGraphics3D.h"
 #include "DirectThreadSafe.h"
 
-//Include Cerasus Common Header File
+// Include Cerasus Common Header File
 #include "CerasusUnit.h"
 
-//Include C/C++ Standard Header File
+// Include C/C++ Standard Header File
 #include <iostream>
 #include <fstream>
 #include <algorithm>
@@ -35,10 +36,10 @@
 #include <queue>
 #include <map>
 
-//Using C/C++ Class Namespace
+// Using C/C++ Class Namespace
 using namespace std;
 
-//Macro Definition
+// Macro Definition
 #define MAX_CONTROL_STATES 6
 
 #define EVENT_BUTTON_CLICKED                0x0101
@@ -52,7 +53,7 @@ using namespace std;
 #define EVENT_LISTBOX_SELECTION             0x0702
 #define EVENT_LISTBOX_SELECTION_END         0x0703
 
-//Enum Definition
+// Enum Definition
 enum SAKURA_CONTROL_TYPE
 {
 	SAKURA_CONTROL_BUTTON,
@@ -87,14 +88,14 @@ enum SAKURA_SCROLLBAR_STATE
 	SAKURA_SCROLLBAR_STATE_HELD_DOWN,
 };
 
-//Struct Definition
+// Struct Definition
 typedef struct
 {
 	WCHAR strFontName[MAX_PATH];
 	int nFontSize;
 } CUFont, *LPCUFont;
 
-//Class Definition
+// Class Definition
 class CSakuraResourceManager;
 class CSakuraDialog;
 class CSakuraControl;
@@ -107,7 +108,7 @@ class CSakuraScrollBar;
 class CSakuraSlider;
 class CSakuraUnit;
 
-//Function Declaration
+// Function Declaration
 typedef VOID(CALLBACK*LPCALLBACKSAKURAGUIEVENT)(UINT nEvent, int nControlID, CSakuraControl* pControl, void* pUserContext);
 
 #endif // !__SAKURAUICOMMON_H_

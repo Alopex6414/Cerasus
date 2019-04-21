405270c0d63c6da77282af5a59adf6826ef17215
Alopex6414 <1029535012@qq.com>
2019-04-21 11:33:56 +0800

    Cerasus v1.5.9.179a. modify commit.

--- Cerasus/SakuraUnit.cpp
+++ Cerasus/SakuraUnit.cpp
@@ -1,13 +1,14 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Team Gorgeous Bubble
 *     All rights reserved.
 *
 * @file		SakuraUnit.cpp
 * @brief	This File is SakuraGUI DLL Project.
-* @author	Alopex/Helium
-* @version	v1.00a
+* @author	Alopex/Alice
+* @version	v1.01a
 * @date		2019-01-06	v1.00a	alopex	Create Project.
+* @date		2019-04-20	v1.01a	alopex	Add Notes.
 */
 #include "SakuraUnit.h"
 
--- Cerasus/SakuraUnit.h
+++ Cerasus/SakuraUnit.h
@@ -1,24 +1,25 @@
 /*
 *     COPYRIGHT NOTICE
-*     Copyright(c) 2018, Team Shanghai Dream Equinox
+*     Copyright(c) 2017~2019, Team Gorgeous Bubble
 *     All rights reserved.
 *
 * @file		SakuraUnit.h
 * @brief	This File is SakuraGUI DLL Project.
-* @author	Alopex/Helium
-* @version	v1.00a
+* @author	Alopex/Alice
+* @version	v1.01a
 * @date		2019-01-06	v1.00a	alopex	Create Project.
+* @date		2019-04-20	v1.01a	alopex	Add Notes.
 */
 #pragma once
 
 #ifndef __SAKURAUNIT_H_
 #define __SAKURAUNIT_H_
 
-//Include SakuraGUI Common Header File
+// Include SakuraGUI Common Header File
 #include "SakuraUICommon.h"
 #include "SakuraControl.h"
 
-//Macro Definition
+// Macro Definition
 #ifdef	CERASUS_EXPORTS
 #define SAKURAUNIT_API	__declspec(dllexport)
 #else
@@ -27,11 +28,11 @@
 
 #define	SAKURAUNIT_CALLMETHOD	__stdcall
 
-//Class Definition
+// Class Definition
 class SAKURAUNIT_API CSakuraUnit : public CSakuraControl
 {
 public:
-	CSakuraUnit(CSakuraDialog* pDialog = NULL);		// CSakuraUnit 构造函数
+	CSakuraUnit(CSakuraDialog* pDialog = NULL);					// CSakuraUnit 构造函数
 
 	virtual void	SAKURAUNIT_CALLMETHOD	Render();			// CSakuraUnit 渲染控件
 
